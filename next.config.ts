a4ecc3d55c1d10f4e4bd51e97cd3fd3940558348
katspaugh <katspaugh@gmail.com>
2026-01-16 21:25:09 +0100

    Fix GitHub Pages asset paths

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
   basePath: isProduction ? "/code.quest" : undefined,
   assetPrefix: isProduction ? "/code.quest/" : undefined,
   images: {
